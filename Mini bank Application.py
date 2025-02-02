81bfd72f5846057dd1d8bec4b43c41b698278f3b
Shaik Allabakash <139256843+allabakash486@users.noreply.github.com>
2025-02-02 18:18:06 +0530

    School Application

--- Mini bank Application.py
+++ Mini bank Application.py
@@ -1,56 +1,34 @@
-class Canara_bank():
-    Bank_name = 'Canara bank'
-    Branch_name = 'Banglore'
-    Branch_Ifsc = 'CBI01234'
-    Rate_of_intrest = 8
-    def __init__(self,name,Gender,Aadhar,Mobile_number,Pin,Balance):
-        self.name = name
-        self.Gender = Gender
-        self.Aadhar = Aadhar
-        self.Mobile_number = Mobile_number
-        self.Pin = Pin
-        self.Balance = Balance
-    @classmethod
-    def Changing_of_rate_of_intrest(cls):
-        new_Rate_of_intrest = float(input("Enter the new Rate of intrest: "));
-        cls.Rate_of_intrest = new_Rate_of_intrest
-    @staticmethod
-    def pin():
-        password = int(input("enter the pin: "))
-        return password
-    def Cecking_balance(self):        
-        if self.pin() == pin:
-            print(f'Available balance is {self.Balance}')
-        else:
-            print('Wrong pin')
-    def Checking_balance(self):        
-        if self.pin() == self.Pin:
-            print(f'Available balance is {self.Balance}')
-        else:
-            print('Wrong pin')
-    def Withdraw(self):        
-        if self.pin()== self.Pin:
-            Amount_withdraw  = int(input("Enter the withdraw amount :"))
-            if Amount_withdraw <= self.Balance:
-                self.Balance -= Amount_withdraw
-                print(f'{Amount_withdraw} Amount is Sucessfully debited........! ')
-                print(f'Available balance is {self.Balance}......!')
-            else:
-                print('Insufficent amount ....!')
-            print(f'Available balance is {self.Balance}......!')
-        else:
-            print('Wrong pin')
-    def Deposite(self):      
-        if self.pin() == self.Pin:
-            Amount_withdraw  = int(input("Enter the withdraw amount :"))            
-            self.Balance += Amount_withdraw
-            print(f'{Amount_withdraw} Amount is Sucessfully debited........! ')
-            print(f'Available balance is {self.Balance}......!')            
-            print(f'Available balance is {self.Balance}......!')
-        else:
-            print('Wrong pin')
-Applicant1 = Canara_bank('Shaik Allabakash','Male',236759679098,6300299581,3863,15000)
-Applicant1.Checking_balance()
-Applicant1.Withdraw()
-Applicant1.Deposite()
-            
+class School():
+    school_name = 'Urdu High school'
+    contact = 9006300588
+    Head_master = 'Hydar ali'
+    Total_num_of_students = 500
+    number_class_rooms = 18
+    play_ground = 'Not available'
+
+    
+    def __init__(self,name,phn,addrs,standard) -> None:
+        self.name= name
+        self.phn = phn
+        self.addrs = addrs
+        self.standard = standard
+
+    
+    def details(self):
+        print(f'name of the student is {self.name}')
+        print(f'{self.name} parents contact number is {self.phn}')
+        print(f'Student address is {self.addrs}')
+        print(f'studying in  {self.standard}')
+        
+    def Update_phone_number(self):
+        New_phn = int(input('Enter the new number: '))
+        self.phn = New_phn
+        print(f'{self.phn}  is updated successfully...😎')
+
+
+student1 = School('Shaik',6300299581,'10-261 Jagan colony Madanapalli(517325)','10th standard')
+student1.details()
+student2 = School('Ab',83091057444,'10-261 Jagan colony Madanapalli(517325)','8th standard')
+student2.details()
+student2.Update_phone_number()
+student2.details()
