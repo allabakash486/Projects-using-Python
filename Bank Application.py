5375413219ae47a129287785adb2c26a4b33e68f
Shaik Allabakash <139256843+allabakash486@users.noreply.github.com>
2025-02-02 18:11:28 +0530

    Bank Application Project

--- Bank Application.py
+++ Bank Application.py
@@ -1,49 +1,65 @@
-class Indain_bank():
-    Name ='Indian Bank'
-    Barnch = 'Bengaluru'
-    IFSC = 'INBNO123'
-    RAte_of_intrest = 5
-    def __init__(self,Accountant_name,Age,Gender,Pin,Balance,Mobile):
-        self.Accountant_name = Accountant_name
-        self.Age = Age
-        self.Gender = Gender
-        self.Pin= Pin
-        self.Balance = Balance
-        self.Mobile = Mobile
-    @staticmethod
-    def Take_pin():
-        password = int(input('Enter the Pin: '))
-        return password
-    @classmethod
-    def Change_of_ROI(cls):
-        new_ROi = float(input('Enter the new rate of intrest: '))
-        cls.RAte_of_intrest = new_ROi
-    def Withdraw_money(self):
-        if self.Pin == self.Take_pin():
-            withdraw_amount = int(input('Enter the withdraw amount: '))
-            if self.Balance >= withdraw_amount:
-                self.Balance -= withdraw_amount
-                print(f'{withdraw_amount} is Debited successfully !....')
-                print(f'Available balance is {self.Balance} RS')
+class BankOfBaroda:
+    BankName  = 'Bank of Baroda'
+    customers =  15000
+    Branch    = 'Madanapalli'
+    IFSC      = 'BOBNO123498'
+    Address   = '10-387-7 opp RTC Bus Stand  Madanapalli'
+    RateOfIntrest = 7.0
+
+    
+    def __init__(self,name,Aadhar,phn,pan,age,balance,pin):
+        self.name    = name
+        self.Aadhar  = Aadhar
+        self.phn     = phn
+        self.pan     = pan
+        self.age     = age
+        self.balance = balance
+        self.pin     = pin
+
+    def CheckBalance(self):
+        if self.pin == self.Takepin():
+            print(f'Available balance is {self.balance}')
+        else:
+            print('Invalid Pin')
+
+
+    def WithdrawAmount(self):
+        if self.pin==self.Takepin():
+            Amount = int(input('Enter the Amount : '))
+            if Amount <= self.balance:
+                self.balance -= Amount
+                print(f'From Your Account {Amount} is debited is Successfully... \n Available balance is {self.balance}')
             else:
-                print('Insufficient funds!...')
+                print('Insufficient Amount')
         else:
-            print('Invalid pin !....')
-    def Deposite(self):
-        if self.Pin == self.Take_pin():
-            Deposite_amount = int(input('Enter the withdraw amount: '))
-            self.Balance += Deposite_amount
-            print(f'{Deposite_amount} is Credited successfully !....')
-            print(f'Available balance is {self.Balance} RS')
+            print('Invalid Pin')
+
+
+    def DepositeAmount(self):
+        if self.pin==self.Takepin():
+            Amount = int(input('Enter the Amount : '))
+            if Amount <= self.balance:
+                self.balance += Amount
+                print(f'From Your Account {Amount} is Credited is Successfully... \n Available balance is {self.balance}')
+            else:
+                print('Insufficient Amount')
         else:
-            print('Invalid pin !....')
-    def Checking_balance(self):
-        if self.Pin == self.Take_pin():
-            print(f'Available balance is {self.Balance} RS')
-        else:
-            print('Invalid pin .....')
-user1 = Indain_bank('shaik',19,'male',6300,1598746321,90006300855)
-user1.Checking_balance()
-user1.Withdraw_money()
-user1.Deposite()
+            print('Invalid Pin')
+
+
+    @staticmethod
+    def Takepin():
+        password = int(input('Enter your Pin :'))
+        return password
+
+
+
+    @classmethod
+    def ChangeOfRateOfIntrest(cls):
+        new_roi = float(input('Enter the New Rate of Intrest  : '))
+        cls.RateOfIntrest = new_roi
+
+Applicant1 = BankOfBaroda('Shaik',236759679098,6300299581,'SAB12486',22,25000,1435)
+Applicant1.DepositeAmount()
+        
     
